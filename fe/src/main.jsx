3c8e45f0faf36b8cef372586b383860b1b5c594d
Nguyễn Tấn Phi <nq2018.nguyentanphi311@gmail>
2025-12-16 15:19:05 +0700

    update

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom"; 
-import { AuthProvider } from "./Context/authContext.jsx"; 
+import { AuthProvider } from "./context/authContext.jsx"; 
 
 import "./index.css";
 
