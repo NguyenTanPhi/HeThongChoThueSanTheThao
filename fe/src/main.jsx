29edc6943fee1f2b6b1f5bf2b29a450d1cf43643
Nguyễn Tấn Phi <nq2018.nguyentanphi311@gmail>
2025-12-30 13:15:49 +0700

    update

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom"; 
-import { AuthProvider } from "./context/authContext.jsx"; 
+import { authProvider } from "./context/authContext.jsx"; 
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider> {/* bọc AuthProvider trước BrowserRouter */}
+    <authProvider> {/* bọc AuthProvider trước BrowserRouter */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </AuthProvider>
+    </authProvider>
   </React.StrictMode>
 );
